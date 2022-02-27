e1caf4b8be5a867c721a22cad3921de33503a872
Shinero <64125211+Sarthak2143@users.noreply.github.com>
2022-02-27 17:04:38 +0530

    removed keyboardinterrupt error.

--- __main__.py
+++ __main__.py
@@ -1,22 +1,28 @@
 import shin
-from colorama import Fore, Style
-
+import time
+from colorama import Fore, Style                        
 print("Shin Lang v0.2 | Author: Shinero")
 
 #for green "shin" and red ">>>"
-while True:
-    text = input(Fore.GREEN + 'shin' + Fore.RED  + ' >>> ' + Style.RESET_ALL)
+try:
 
-    if text.strip() == "": continue
+    while True:
+        text = input(Fore.GREEN + 'shin' + Fore.RED  + ' >>> ' + Style.RESET_ALL)
 
-    result, error = shin.run('<stdin>', text)
+        if text.strip() == "": continue
 
-    if error:
-        print(error.as_string())
+        result, error = shin.run('<stdin>', text)
 
-    elif result:
-        if len(result.elements) == 1:
+        if error:
+            print(error.as_string())
+
+        elif result:
+            if len(result.elements) == 1:
                 print(repr(result.elements[0]))
 
-        else:
-            print(repr(result))
+            else:
+                print(repr(result))
+
+except KeyboardInterrupt:
+    print('\nExiting...')
+    time.sleep(1)
